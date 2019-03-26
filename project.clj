17555ff8c165ba5bbfef193137186597753fa656
julia.cicale <julia.cicale@vodori.com>
2019-03-26 16:20:35 -0500

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.vodori/schema-forms "0.1.0"
+(defproject com.vodori/schema-forms "0.1.1-SNAPSHOT"
 
   :description
   "A library for converting prismatic schemas into json-schemas for building form fields."
